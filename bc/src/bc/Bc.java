35d73d4490a9ea0db9f699948d3e008e5af2f540
michy70 <michy70@users.noreply.github.com>
2020-03-05 15:10:20 +0100

    prvy commit

---
+++ bc/src/bc/Bc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bc;
+
+/**
+ *
+ * @author michy70
+ */
+public class Bc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
